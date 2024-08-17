39c035a897aef1560c204cf139d65c4822b150bc
Yuchin Juan <juan.yuchin@gmail.com>
2024-08-17 07:57:22 +0000

    topk with bucket sort

---
+++ topk_with_bucket_sort/main.cu
@@ -0,0 +1,99 @@
+#include <random>
+#include <vector>
+#include <stdexcept>
+#include <iostream>
+
+#include "common.cuh"
+#include "topk.cuh"
+
+using namespace std;
+
+#define CHECK_CUDA(func)                                                                                                           \
+    {                                                                                                                              \
+        cudaError_t status = (func);                                                                                               \
+        if (status != cudaSuccess)                                                                                                 \
+        {                                                                                                                          \
+            string error = "[main.cu] CUDA API failed at line " + to_string(__LINE__) + " with error: " + cudaGetErrorString(status) + "\n"; \
+            throw runtime_error(error);                                                                                            \
+        }                                                                                                                          \
+    }
+
+int kNumToRetrieve = 1000;
+int kNumTrials = 10;
+
+void runExp(int numDocs)
+{
+    cout << "\n\nrunning exps with numDocs: " << numDocs << endl;
+    default_random_engine generator;
+    uniform_real_distribution<float> distribution(-1.0, 1.0);
+
+    vector<Doc> v_doc(numDocs);
+    for (int i = 0; i < numDocs; i++)
+    {
+        v_doc[i].docId = i;
+        v_doc[i].score = distribution(generator);
+    }
+
+    TopkBucketSort retriever;
+    retriever.init();
+    Doc *d_doc = nullptr;
+    Doc *d_buffer = nullptr;
+    CHECK_CUDA(cudaMalloc(&d_doc, numDocs * sizeof(Doc)));
+    CHECK_CUDA(cudaMalloc(&d_buffer, numDocs * sizeof(Doc)));
+
+    double timeMsGpuBucketSort = 0;
+    double timeMsGpuFullSort = 0;
+    double timeMsCpuFullSort = 0;
+    for (int t = -3; t < kNumTrials; t++)
+    {
+        float timeMsGpuBucketSort1 = 0;
+        float timeMsGpuFullSort1 = 0;
+        float timeMsCpuFullSort1 = 0;
+        CHECK_CUDA(cudaMemcpy(d_doc, v_doc.data(), numDocs * sizeof(Doc), cudaMemcpyHostToDevice));
+        vector<Doc> v_topk_gpuBucketSort = retriever.retrieveTopk(d_doc, d_buffer, numDocs, kNumToRetrieve, timeMsGpuBucketSort1);
+        CHECK_CUDA(cudaMemcpy(d_doc, v_doc.data(), numDocs * sizeof(Doc), cudaMemcpyHostToDevice));
+        vector<Doc> v_topk_gpuFullSort = retrieveTopkGpuFullSort(d_doc, numDocs, kNumToRetrieve, timeMsGpuFullSort1);
+        vector<Doc> v_doc_copy = v_doc;
+        vector<Doc> v_topk_cpuFullSort = retrieveTopkCpuFullSort(v_doc_copy, kNumToRetrieve, timeMsCpuFullSort1);
+
+        if (v_topk_gpuBucketSort != v_topk_gpuFullSort)
+        {
+            throw runtime_error("Topk results from GPU bucket sort and GPU full sort do not match");
+        }
+        if (v_topk_gpuBucketSort != v_topk_cpuFullSort)
+        {
+            throw runtime_error("Topk results from GPU bucket sort and CPU full sort do not match");
+        }
+
+        if (t >= 0)
+        {
+            timeMsGpuBucketSort += timeMsGpuBucketSort1;
+            timeMsGpuFullSort += timeMsGpuFullSort1;
+            timeMsCpuFullSort += timeMsCpuFullSort1;
+        }
+    }
+
+    timeMsGpuBucketSort /= kNumTrials;
+    timeMsGpuFullSort /= kNumTrials;
+    timeMsCpuFullSort /= kNumTrials;
+
+    cout << "timeMsGpuBucketSort: " << timeMsGpuBucketSort << " ms" << endl;
+    cout << "timeMsGpuFullSort: " << timeMsGpuFullSort << " ms" << endl;
+    cout << "timeMsCpuFullSort: " << timeMsCpuFullSort << " ms" << endl;
+
+    CHECK_CUDA(cudaFree(d_doc));
+    CHECK_CUDA(cudaFree(d_buffer));
+    retriever.reset();
+}
+
+int main()
+{
+    runExp(1000000);
+    runExp(2000000);
+    runExp(4000000);
+    runExp(8000000);
+    runExp(16000000);
+    runExp(32000000);
+
+    return 0;
+}
---
+++ topk_with_bucket_sort/topk.cuh
@@ -0,0 +1,68 @@
+#ifndef TOPK_CUH
+#define TOPK_CUH
+
+#include <vector>
+
+#include "common.cuh"
+
+class TopkBucketSort
+{
+public:
+    void init();
+
+    void reset();
+
+    std::vector<Doc> retrieveTopk(Doc *d_doc, Doc *d_buffer, int numDocs, int numToRetrieve, float &timeMs);
+
+    __device__ __host__ int getCounterIdx(int slot, int bucket)
+    {
+        return slot * kNumBuckets_ + bucket;
+    }
+
+    __device__ int bucketize(float score)
+    {
+        score = min(kMaxScore_, max(kMinScore_, score));
+        score = (score - kMinScore_) / (kMaxScore_ - kMinScore_);
+        return (int)(score * kGranularity_);
+    }
+
+    __device__ void updateCounter(Doc doc)
+    {
+        int slot = doc.docId % kNumSlots_;
+        int bucket = bucketize(doc.score);
+        int counterIdx = getCounterIdx(slot, bucket);
+
+        atomicAdd(&d_counter_[counterIdx], 1);
+    }
+
+    __device__ bool operator()(const Doc &doc)
+    {
+        int bucket = bucketize(doc.score);
+        return bucket >= lowestBucket_;
+    }
+
+private:
+    const float kMinScore_ = -1;
+    const float kMaxScore_ = 1;
+    const int kGranularity_ = 512;
+    const int kNumBuckets_ = kGranularity_ + 1;
+    // For example, if you have kMinScore = -1.0, kMaxScore = 1.0, kGranularity = 2,
+    // then you will have 3 buckets: [-1, 0, 1]
+    const int kNumSlots_ = 16;
+    // Each bucket has 16 slots. A GPU thread will randomly write into one of the thread,
+    // and then the count will finally be accumulated to the first slot.
+    // The purpose of doing this is to minimize too much concurrent write to the same memory location with atomicAdd.
+    const int kSize_d_counter_ = kNumBuckets_ * kNumSlots_;
+    const int kSize_byte_d_counter_ = kSize_d_counter_ * sizeof(int);
+
+    int *d_counter_ = nullptr;
+    int *h_counter_ = nullptr;
+    int lowestBucket_ = 0;
+
+    void findLowestBucket(std::vector<int> &v_counter, int numToRetrieve, int &lowestBucket, int &numDocsGreaterThanLowestBucket);
+};
+
+std::vector<Doc> retrieveTopkGpuFullSort(Doc *d_doc, int numDocs, int numToRetrieve, float &timeMs);
+std::vector<Doc> retrieveTopkCpuFullSort(std::vector<Doc> &v_doc, int numToRetrieve, float &timeMs);
+
+#endif // TOPK_CUH
