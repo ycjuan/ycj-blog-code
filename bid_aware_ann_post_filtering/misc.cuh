12f69a85c682a8b25d1d507db079c8f23d81e522
Yuchin Juan <juan.yuchin@gmail.com>
2024-10-28 00:40:09 +0000

    improve getUpperAndLowerBound efficiency

--- bid_aware_ann_post_filtering/misc.cuh
+++ bid_aware_ann_post_filtering/misc.cuh
@@ -7,6 +7,7 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <random>
 
 using namespace std;
 
@@ -15,12 +16,46 @@ inline void getUpperAndLowerBound(const std::vector<ItemCpu> &req1D,
                                   float &minScore,
                                   float &maxScore)
 {
-    vector<float> scores;
-    for (auto req : req1D)
+    int kMaxNumReqs = 100;
+    int kMaxNumDocs = 100000;
+
+    vector<int> reqIdx1D(req1D.size());
+    for (int i = 0; i < req1D.size(); i++)
     {
-        for (auto doc : doc1D)
+        reqIdx1D[i] = req1D[i].uid;
+    }
+
+    vector<int> docIdx1D(doc1D.size());
+    for (int i = 0; i < doc1D.size(); i++)
+    {
+        docIdx1D[i] = doc1D[i].uid;
+    }
+
+    shuffle(reqIdx1D.begin(), reqIdx1D.end(), default_random_engine(0));
+    shuffle(docIdx1D.begin(), docIdx1D.end(), default_random_engine(0));
+
+    if (reqIdx1D.size() > kMaxNumReqs)
+    {
+        reqIdx1D.resize(kMaxNumReqs);
+    }
+
+    if (docIdx1D.size() > kMaxNumDocs)
+    {
+        docIdx1D.resize(kMaxNumDocs);
+    }
+
+    sort(reqIdx1D.begin(), reqIdx1D.end());
+    sort(docIdx1D.begin(), docIdx1D.end());
+
+    vector<float> scores(reqIdx1D.size() * docIdx1D.size());
+    #pragma omp parallel for
+    for (int i = 0; i < reqIdx1D.size(); i++)
+    {
+        for (int j = 0; j < docIdx1D.size(); j++)
         {
-            scores.push_back(getScore(req, doc));
+            int reqIdx = reqIdx1D[i];
+            int docIdx = docIdx1D[j];
+            scores[i * docIdx1D.size() + j] = getScore(req1D[reqIdx], doc1D[docIdx]);
         }
     }
 
@@ -28,6 +63,8 @@ inline void getUpperAndLowerBound(const std::vector<ItemCpu> &req1D,
 
     minScore = scores[(int)(scores.size() * 0.002)];
     maxScore = scores[(int)(scores.size() * 0.998)];
+
+    cout << "minScore: " << minScore << ", maxScore: " << maxScore << endl;
 }
 
 inline void printLatency(const vector<float> &latency1D, const string &prefix)
