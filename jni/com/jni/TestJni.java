5bc8858990762a099193d9cb8407b2d31611c3aa
Yuchin Juan <juan.yuchin@gmail.com>
2024-11-10 22:00:31 +0000

    add JNI benchmark

--- jni/com/jni/TestJni.java
+++ jni/com/jni/TestJni.java
@@ -1,24 +1,38 @@
 package com.jni;
 
+import java.lang.Math;
+
 public class TestJni {
+
+    private static int SHAPE_0 = 16;
+    private static int SHAPE_1 = 128;
 
     private static InputClass constructInput() {
         InputClass input = new InputClass();
-        input.inputField0D = 1;
-        input.inputField1D = new int[] {11, 12, 13};
-        input.inputField2D = new int[][] {{21, 22, 23}, {24, 25, 26}};
+        int uniqueInt = 1;
+        input.inputField0D = uniqueInt++;
+        input.inputField1D = new int[SHAPE_1];
+        for (int i = 0; i < SHAPE_1; i++) {
+            input.inputField1D[i] = uniqueInt++;
+        }
+        input.inputField2D = new int[SHAPE_0][SHAPE_1];
+        for (int i = 0; i < SHAPE_0; i++) {
+            for (int j = 0; j < SHAPE_1; j++) {
+                input.inputField2D[i][j] = uniqueInt++;
+            }
+        }
         input.inputFieldInner = new InputClassInner();
-        input.inputFieldInner.inputFieldInner0D = 31;
+        input.inputFieldInner.inputFieldInner0D = uniqueInt++;
         return input;
     }
 
     private static void printOutput(OutputClass output) {
         System.out.println("outputField0D: " + output.outputField0D);
-        for (int i = 0; i < output.outputField1D.length; i++) {
+        for (int i = 0; i < Math.min(3, output.outputField1D.length); i++) {
             System.out.println("outputField1D[" + i + "]: " + output.outputField1D[i]);
         }
-        for (int i = 0; i < output.outputField2D.length; i++) {
-            for (int j = 0; j < output.outputField2D[i].length; j++) {
+        for (int i = 0; i < Math.min(3, output.outputField2D.length); i++) {
+            for (int j = 0; j < Math.min(3, output.outputField2D[i].length); j++) {
                 System.out.println("outputField2D[" + i + "][" + j + "]: " + output.outputField2D[i][j]);
             }
         }
--- jni/jni.cc
+++ jni/jni.cc
@@ -1,9 +1,12 @@
 #include "com_jni_JniMain.h"
+#include "util.h"
 
 #include <vector>
 #include <iostream>
 
 using namespace std;
+
+const int kNumTrials = 100;
 
 // data structures
 struct Input
@@ -22,16 +25,33 @@ struct Output
     int outputFieldInner0D;
 };
 
-// parse input functions
-void parseInput0D(JNIEnv *jenv, jobject jobj_input, Input &input)
+struct TimerRecord
 {
+    long timeMicroSecParseInput0D = 0;
+    long timeMicroSecParseInput1D = 0;
+    long timeMicroSecParseInput2D = 0;
+    long timeMicroSecParseInputInner = 0;
+    long timeMicroSecUpdateOutput0D = 0;
+    long timeMicroSecUpdateOutput1D = 0;
+    long timeMicroSecUpdateOutput2D = 0;
+    long timeMicroSecUpdateOutputInner = 0;
+};
+
+// parse input functions
+void parseInput0D(JNIEnv *jenv, jobject jobj_input, Input &input, TimerRecord &timerRecord)
+{
+    Timer timer;
+    timer.tic();
     jclass jcls_input = jenv->GetObjectClass(jobj_input);
     jfieldID jfid_inputField0D = jenv->GetFieldID(jcls_input, "inputField0D", "I");
     input.inputField0D = jenv->GetIntField(jobj_input, jfid_inputField0D);
+    timerRecord.timeMicroSecParseInput0D += timer.tocMicroSec();
 }
 
-void parseInput1D(JNIEnv *jenv, jobject jobj_input, Input &input)
+void parseInput1D(JNIEnv *jenv, jobject jobj_input, Input &input, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_input = jenv->GetObjectClass(jobj_input);
     jfieldID jfid_inputField1D = jenv->GetFieldID(jcls_input, "inputField1D", "[I");
     jintArray jarr_inputField1D = (jintArray)jenv->GetObjectField(jobj_input, jfid_inputField1D);
@@ -39,10 +59,13 @@ void parseInput1D(JNIEnv *jenv, jobject jobj_input, Input &input)
     input.inputField1D.resize(jsize_inputField1D);
     jenv->GetIntArrayRegion(jarr_inputField1D, 0, jsize_inputField1D, input.inputField1D.data());
     jenv->DeleteLocalRef(jarr_inputField1D);
+    timerRecord.timeMicroSecParseInput1D += timer.tocMicroSec();
 }
 
-void parseInput2D(JNIEnv *jenv, jobject jobj_input, Input &input)
+void parseInput2D(JNIEnv *jenv, jobject jobj_input, Input &input, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_input = jenv->GetObjectClass(jobj_input);
     jfieldID jfid_inputField2D = jenv->GetFieldID(jcls_input, "inputField2D", "[[I");
     jobjectArray jarr_inputField2D = (jobjectArray)jenv->GetObjectField(jobj_input, jfid_inputField2D);
@@ -57,11 +80,14 @@ void parseInput2D(JNIEnv *jenv, jobject jobj_input, Input &input)
         jenv->DeleteLocalRef(jarr_inputField2D_i);
     }
     jenv->DeleteLocalRef(jarr_inputField2D);
+    timerRecord.timeMicroSecParseInput2D += timer.tocMicroSec();
 }
 
 // reference: https://stackoverflow.com/questions/32513413/jni-getfieldid-returning-null-for-inner-class
-void parseInputInner(JNIEnv *env, jobject jobj_input, Input &input)
+void parseInputInner(JNIEnv *env, jobject jobj_input, Input &input, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_input = env->GetObjectClass(jobj_input);
     jfieldID jfieldID_innerFieldInner = env->GetFieldID(jcls_input, "inputFieldInner", "Lcom/jni/InputClassInner;");
     jobject jobj_innerFieldInner = env->GetObjectField(jobj_input, jfieldID_innerFieldInner);
@@ -76,29 +102,38 @@ void parseInputInner(JNIEnv *env, jobject jobj_input, Input &input)
     input.inputFieldInner0D = env->GetIntField(jobj_innerFieldInner, jfieldID_inputFieldInner0D);
 
     env->DeleteLocalRef(jobj_innerFieldInner);
+    timerRecord.timeMicroSecParseInputInner += timer.tocMicroSec();
 }
 
 
 // update output functions
-void updateOutput0D(JNIEnv *jenv, jobject jobj_output, Output &output)
+void updateOutput0D(JNIEnv *jenv, jobject jobj_output, Output &output, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_output = jenv->GetObjectClass(jobj_output);
     jfieldID jfid_outputField0D = jenv->GetFieldID(jcls_output, "outputField0D", "I");
     jenv->SetIntField(jobj_output, jfid_outputField0D, output.outputField0D);
+    timerRecord.timeMicroSecUpdateOutput0D += timer.tocMicroSec();
 }
 
-void updateOutput1D(JNIEnv *jenv, jobject jobj_output, Output &output)
+void updateOutput1D(JNIEnv *jenv, jobject jobj_output, Output &output, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_output = jenv->GetObjectClass(jobj_output);
     jfieldID jfid_outputField1D = jenv->GetFieldID(jcls_output, "outputField1D", "[I");
     jintArray jarr_outputField1D = jenv->NewIntArray(output.outputField1D.size());
     jenv->SetIntArrayRegion(jarr_outputField1D, 0, output.outputField1D.size(), output.outputField1D.data());
     jenv->SetObjectField(jobj_output, jfid_outputField1D, jarr_outputField1D);
     jenv->DeleteLocalRef(jarr_outputField1D);
+    timerRecord.timeMicroSecUpdateOutput1D += timer.tocMicroSec();
 }
 
-void updateOutput2D(JNIEnv *jenv, jobject jobj_output, Output &output)
+void updateOutput2D(JNIEnv *jenv, jobject jobj_output, Output &output, TimerRecord &timerRecord)
 {
+    Timer timer;
+    timer.tic();
     jclass jcls_output = jenv->GetObjectClass(jobj_output);
     jfieldID jfid_outputField2D = jenv->GetFieldID(jcls_output, "outputField2D", "[[I");
     jobjectArray jarr_outputField2D = jenv->NewObjectArray(output.outputField2D.size(), jenv->FindClass("[I"), NULL);
@@ -111,10 +146,13 @@ void updateOutput2D(JNIEnv *jenv, jobject jobj_output, Output &output)
     }
     jenv->SetObjectField(jobj_output, jfid_outputField2D, jarr_outputField2D);
     jenv->DeleteLocalRef(jarr_outputField2D);
+    timerRecord.timeMicroSecUpdateOutput2D += timer.tocMicroSec();
 }
 
-void updateOutputInner(JNIEnv *env, jobject jobj_output, Output &output)
+void updateOutputInner(JNIEnv *env, jobject jobj_output, Output &output, TimerRecord &timerRecord)
 {   
+    Timer timer;
+    timer.tic();
     jclass jcls_outputClassInner = env->FindClass("com/jni/OutputClassInner");
     jobject jobj_outputClassInner = env->AllocObject(jcls_outputClassInner);
 
@@ -126,6 +164,7 @@ void updateOutputInner(JNIEnv *env, jobject jobj_output, Output &output)
     env->SetObjectField(jobj_output, jfieldID_outputClassInner, jobj_outputClassInner);
 
     env->DeleteLocalRef(jobj_outputClassInner);
+    timerRecord.timeMicroSecUpdateOutputInner += timer.tocMicroSec();
 }
 
 // Core class
@@ -142,16 +181,16 @@ public:
 
         // process outputField1D
         output.outputField1D = input.inputField1D;
-        for (int i = 0; i < input.inputField1D.size(); i++)
+        for (int i = 0; i < min(3UL, input.inputField1D.size()); i++)
         {
             cout << "inputField1D[" << i << "]: " << input.inputField1D[i] << endl;
         }
         
         // process outputField2D
         output.outputField2D = input.inputField2D;
-        for (int i = 0; i < input.inputField2D.size(); i++)
+        for (int i = 0; i < min(3UL, input.inputField2D.size()); i++)
         {
-            for (int j = 0; j < input.inputField2D[i].size(); j++)
+            for (int j = 0; j < min(3UL, input.inputField2D[i].size()); j++)
             {
                 cout << "inputField2D[" << i << "][" << j << "]: " << input.inputField2D[i][j] << endl;
             }
@@ -176,21 +215,38 @@ JNIEXPORT jobject JNICALL Java_com_jni_JniMain_c_1process
   (JNIEnv * jenv, jobject, jlong jlong_corePtr, jobject jobj_input)
 {
     Core &core = *((Core*)jlong_corePtr);
+    TimerRecord timerRecord;
 
     Input input;
-    parseInput0D(jenv, jobj_input, input);
-    parseInput1D(jenv, jobj_input, input);
-    parseInput2D(jenv, jobj_input, input);
-    parseInputInner(jenv, jobj_input, input);
+    for (int i = 0; i < kNumTrials; i++)
+    {
+        parseInput0D(jenv, jobj_input, input, timerRecord);
+        parseInput1D(jenv, jobj_input, input, timerRecord);
+        parseInput2D(jenv, jobj_input, input, timerRecord);
+        parseInputInner(jenv, jobj_input, input, timerRecord);
+    }
 
     Output output = core.process(input);
 
     jclass jcls_output = jenv->FindClass("com/jni/OutputClass");
     jobject jobj_output = jenv->AllocObject(jcls_output);
-    updateOutput0D(jenv, jobj_output, output);
-    updateOutput1D(jenv, jobj_output, output);
-    updateOutput2D(jenv, jobj_output, output);
-    updateOutputInner(jenv, jobj_output, output);
+
+    for (int i = 0; i < kNumTrials; i++)
+    {
+        updateOutput0D(jenv, jobj_output, output, timerRecord);
+        updateOutput1D(jenv, jobj_output, output, timerRecord);
+        updateOutput2D(jenv, jobj_output, output, timerRecord);
+        updateOutputInner(jenv, jobj_output, output, timerRecord);
+    }
+
+    cout << "timeMicroSecParseInput0D: " << (double)timerRecord.timeMicroSecParseInput0D / kNumTrials << endl;
+    cout << "timeMicroSecParseInput1D: " << (double)timerRecord.timeMicroSecParseInput1D / kNumTrials << endl;
+    cout << "timeMicroSecParseInput2D: " << (double)timerRecord.timeMicroSecParseInput2D / kNumTrials << endl;
+    cout << "timeMicroSecParseInputInner: " << (double)timerRecord.timeMicroSecParseInputInner / kNumTrials << endl;
+    cout << "timeMicroSecUpdateOutput0D: " << (double)timerRecord.timeMicroSecUpdateOutput0D / kNumTrials << endl;
+    cout << "timeMicroSecUpdateOutput1D: " << (double)timerRecord.timeMicroSecUpdateOutput1D / kNumTrials << endl;
+    cout << "timeMicroSecUpdateOutput2D: " << (double)timerRecord.timeMicroSecUpdateOutput2D / kNumTrials << endl;
+    cout << "timeMicroSecUpdateOutputInner: " << (double)timerRecord.timeMicroSecUpdateOutputInner / kNumTrials << endl;
 
     return jobj_output;
 }
