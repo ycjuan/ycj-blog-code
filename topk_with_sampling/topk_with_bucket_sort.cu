5c188f915b5f53493f0f62ae8691d0190feabca4
Yuchin Juan <juan.yuchin@gmail.com>
2024-11-24 07:50:36 +0000

    copy

---
+++ topk_with_sampling/topk_with_bucket_sort.cu
@@ -0,0 +1,119 @@
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <cassert>
+#include <numeric>
+#include <thrust/copy.h>
+#include <thrust/execution_policy.h>
+#include <thrust/sort.h>
+#include <string>
+
+#include "topk.cuh"
+#include "common.cuh"
+#include "util.cuh"
+
+using namespace std;
+
+#define CHECK_CUDA(func)                                                                                                           \
+    {                                                                                                                              \
+        cudaError_t status = (func);                                                                                               \
+        if (status != cudaSuccess)                                                                                                 \
+        {                                                                                                                          \
+            string error = "[topk_with_bucket_sort.cu] CUDA API failed at line " + to_string(__LINE__) + " with error: " + cudaGetErrorString(status) + "\n"; \
+            throw runtime_error(error);                                                                                            \
+        }                                                                                                                          \
+    }
+
+void TopkBucketSort::init()
+{
+    CHECK_CUDA(cudaMalloc(&d_counter_, kNumBuckets_ * kNumSlots_ * sizeof(int)));
+    CHECK_CUDA(cudaMallocHost(&h_counter_, kNumBuckets_ * kNumSlots_ * sizeof(int)));
+}
+
+void TopkBucketSort::reset()
+{
+    CHECK_CUDA(cudaFree(d_counter_));
+    CHECK_CUDA(cudaFreeHost(h_counter_));
+}
+
+__global__ void updateCounterKernel(Doc *d_doc, int numDocs, TopkBucketSort retriever)
+{
+    int docId = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (docId < numDocs)
+    {
+        Doc doc = d_doc[docId];
+        retriever.updateCounter(doc);
+    }
+}
+
+void TopkBucketSort::findLowestBucket(vector<int> &v_counter, int numToRetrieve, int &lowestBucket, int &numDocsGreaterThanLowestBucket)
+{
+    lowestBucket = 0;
+    numDocsGreaterThanLowestBucket = 0;
+    // Starting from the highest bucket, accumulate the count until it satisfies numToRetrieve
+    for (int bucket = kGranularity_; bucket >= 0; bucket--)
+    {
+        // Accumulate the count of all slots into the first slot
+        int slot0 = 0;
+        int counterIdx0 = getCounterIdx(slot0, bucket);
+        for (int slot = 1; slot < kNumSlots_; slot++)
+        {
+            int counterIdx = getCounterIdx(slot, bucket);
+            v_counter[counterIdx0] += v_counter[counterIdx];
+        }
+        numDocsGreaterThanLowestBucket += v_counter[counterIdx0];
+        if (numDocsGreaterThanLowestBucket >= numToRetrieve)
+        {
+            lowestBucket = bucket;
+            break;
+        }
+    }
+}
+
+vector<Doc> TopkBucketSort::retrieveTopk(Doc *d_doc, Doc *d_buffer, int numDocs, int numToRetrieve, float &timeMs)
+{
+    CudaTimer timer;
+    timer.tic();
+
+    int kBlockSize = 256;
+    int gridSize = (int)ceil((double)(numDocs + 1) / kBlockSize);
+
+    // Step1 - Run kernel to update the counter
+    CHECK_CUDA(cudaMemset(d_counter_, 0, kSize_byte_d_counter_))
+    updateCounterKernel<<<gridSize, kBlockSize>>>(d_doc, numDocs, *this);
+    cudaDeviceSynchronize();
+    CHECK_CUDA(cudaGetLastError())
+
+    // Step2 - Copy counter from GPU to CPU
+    vector<int> v_counter(kSize_d_counter_, 0);
+    CHECK_CUDA(cudaMemcpy(v_counter.data(), d_counter_, kSize_byte_d_counter_, cudaMemcpyDeviceToHost))
+
+    // Step3 - Find the lowest bucket
+    int numDocsGreaterThanLowestBucket;
+    findLowestBucket(v_counter, numToRetrieve, lowestBucket_, numDocsGreaterThanLowestBucket);
+
+    // Step4 - Filter items that is larger than the lowest bucket
+    Doc *d_endPtr = thrust::copy_if(thrust::device, d_doc, d_doc + numDocs, d_buffer, *this); // copy_if will call TopkBucketSort::operator()
+    cudaDeviceSynchronize();
+    CHECK_CUDA(cudaGetLastError())
+    int numCopied = (d_endPtr - d_buffer);
+    assert(numCopied == numDocsGreaterThanLowestBucket);
+
+    // Step5 - Only sort the docs that are larger than the lowest bucket
+    thrust::stable_sort(thrust::device, d_buffer, d_buffer + numCopied, ScorePredicator());
+    cudaDeviceSynchronize();
+    CHECK_CUDA(cudaGetLastError())
+
+    // Step6 - copy back to CPU
+    vector<Doc> v_doc(numToRetrieve);
+    CHECK_CUDA(cudaMemcpy(v_doc.data(), d_buffer, sizeof(Doc) * numToRetrieve, cudaMemcpyDeviceToHost))
+
+    timeMs = timer.tocMs();
+
+    return v_doc;
+}
