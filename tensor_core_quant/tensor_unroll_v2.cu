4ca26d844de3a1f649b2f43783e95e04f07e5afe
Yuchin Juan <juan.yuchin@gmail.com>
2025-01-18 21:23:03 +0000

    8 x 1 works

--- tensor_core_quant/tensor_unroll_v2.cu
+++ tensor_core_quant/tensor_unroll_v2.cu
@@ -47,33 +47,33 @@ __global__ void quantWmmaUnrollKernelV2(const unsigned *a, const unsigned *b, in
    wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag1;
    wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag2;
    wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag3;
-   //wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag4;
-   //wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag5;
-   //wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag6;
-   //wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag7;
+   wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag4;
+   wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag5;
+   wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag6;
+   wmma::fragment<wmma::matrix_a, 8, 8, 128, precision::b1, wmma::row_major> a_frag7;
    wmma::fragment<wmma::matrix_b, 8, 8, 128, precision::b1, wmma::col_major> b_frag0;
    wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag00;
    wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag10;
    wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag20;
    wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag30;
-   //wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag40;
-   //wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag50;
-   //wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag60;
-   //wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag70;
+   wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag40;
+   wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag50;
+   wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag60;
+   wmma::fragment<wmma::accumulator, 8, 8, 128, int> c_frag70;
    wmma::fill_fragment(c_frag00, 0);
    wmma::fill_fragment(c_frag10, 0);
    wmma::fill_fragment(c_frag20, 0);
    wmma::fill_fragment(c_frag30, 0);
-   //wmma::fill_fragment(c_frag40, 0);
-   //wmma::fill_fragment(c_frag50, 0);
-   //wmma::fill_fragment(c_frag60, 0);
-   //wmma::fill_fragment(c_frag70, 0);
+   wmma::fill_fragment(c_frag40, 0);
+   wmma::fill_fragment(c_frag50, 0);
+   wmma::fill_fragment(c_frag60, 0);
+   wmma::fill_fragment(c_frag70, 0);
 
    size_t lda32 = lda / 32;
    size_t ldb32 = ldb / 32;
    for (int i = 0; i < K; i += WMMA_K) {
       int i32 = i / 32;
-      size_t aRow0 = warpM * WMMA_M * 4;
+      size_t aRow0 = warpM * WMMA_M * 8;
       size_t aCol0 = i32;
 
       size_t aRow1 = aRow0 + WMMA_M;
@@ -85,17 +85,17 @@ __global__ void quantWmmaUnrollKernelV2(const unsigned *a, const unsigned *b, in
       size_t aRow3 = aRow0 + WMMA_M * 3;
       size_t aCol3 = i32;
 
-      //size_t aRow4 = aRow0 + WMMA_M * 4;
-      //size_t aCol4 = i32;
+      size_t aRow4 = aRow0 + WMMA_M * 4;
+      size_t aCol4 = i32;
 
-      //size_t aRow5 = aRow0 + WMMA_M * 5;
-      //size_t aCol5 = i32;
+      size_t aRow5 = aRow0 + WMMA_M * 5;
+      size_t aCol5 = i32;
 
-      //size_t aRow6 = aRow0 + WMMA_M * 6;
-      //size_t aCol6 = i32;
+      size_t aRow6 = aRow0 + WMMA_M * 6;
+      size_t aCol6 = i32;
 
-      //size_t aRow7 = aRow0 + WMMA_M * 7;
-      //size_t aCol7 = i32;
+      size_t aRow7 = aRow0 + WMMA_M * 7;
+      size_t aCol7 = i32;
 
       size_t bRow0 = i32;
       size_t bCol0 = warpN * WMMA_N * 1;
@@ -104,10 +104,10 @@ __global__ void quantWmmaUnrollKernelV2(const unsigned *a, const unsigned *b, in
       wmma::load_matrix_sync(a_frag1, a + aRow1 * lda32 + aCol1, lda);
       wmma::load_matrix_sync(a_frag2, a + aRow2 * lda32 + aCol2, lda);
       wmma::load_matrix_sync(a_frag3, a + aRow3 * lda32 + aCol3, lda);
-      //wmma::load_matrix_sync(a_frag4, a + aRow4 * lda32 + aCol4, lda);
-      //wmma::load_matrix_sync(a_frag5, a + aRow5 * lda32 + aCol5, lda);
-      //wmma::load_matrix_sync(a_frag6, a + aRow6 * lda32 + aCol6, lda);
-      //wmma::load_matrix_sync(a_frag7, a + aRow7 * lda32 + aCol7, lda);
+      wmma::load_matrix_sync(a_frag4, a + aRow4 * lda32 + aCol4, lda);
+      wmma::load_matrix_sync(a_frag5, a + aRow5 * lda32 + aCol5, lda);
+      wmma::load_matrix_sync(a_frag6, a + aRow6 * lda32 + aCol6, lda);
+      wmma::load_matrix_sync(a_frag7, a + aRow7 * lda32 + aCol7, lda);
       wmma::load_matrix_sync(b_frag0, b + bCol0 * ldb32 + bRow0, ldb);
 
       // Perform the matrix multiplication
@@ -115,20 +115,20 @@ __global__ void quantWmmaUnrollKernelV2(const unsigned *a, const unsigned *b, in
       wmma::bmma_sync(c_frag10, a_frag1, b_frag0, c_frag10);
       wmma::bmma_sync(c_frag20, a_frag2, b_frag0, c_frag20);
       wmma::bmma_sync(c_frag30, a_frag3, b_frag0, c_frag30);
-      //wmma::bmma_sync(c_frag40, a_frag4, b_frag0, c_frag40);
-      //wmma::bmma_sync(c_frag50, a_frag5, b_frag0, c_frag50);
-      //wmma::bmma_sync(c_frag60, a_frag6, b_frag0, c_frag60);
-      //wmma::bmma_sync(c_frag70, a_frag7, b_frag0, c_frag70);
+      wmma::bmma_sync(c_frag40, a_frag4, b_frag0, c_frag40);
+      wmma::bmma_sync(c_frag50, a_frag5, b_frag0, c_frag50);
+      wmma::bmma_sync(c_frag60, a_frag6, b_frag0, c_frag60);
+      wmma::bmma_sync(c_frag70, a_frag7, b_frag0, c_frag70);
    }
 
-   int cRow0 = warpM * WMMA_M * 4;
+   int cRow0 = warpM * WMMA_M * 8;
    int cRow1 = cRow0 + WMMA_M;
    int cRow2 = cRow0 + WMMA_M * 2;
    int cRow3 = cRow0 + WMMA_M * 3;
-   //int cRow4 = cRow0 + WMMA_M * 4;
-   //int cRow5 = cRow0 + WMMA_M * 5;
-   //int cRow6 = cRow0 + WMMA_M * 6;
-   //int cRow7 = cRow0 + WMMA_M * 7;
+   int cRow4 = cRow0 + WMMA_M * 4;
+   int cRow5 = cRow0 + WMMA_M * 5;
+   int cRow6 = cRow0 + WMMA_M * 6;
+   int cRow7 = cRow0 + WMMA_M * 7;
 
    int cCol0 = warpN * WMMA_N * 1;
 
@@ -139,20 +139,20 @@ __global__ void quantWmmaUnrollKernelV2(const unsigned *a, const unsigned *b, in
       c_frag10.x[i] = K - c_frag10.x[i];
       c_frag20.x[i] = K - c_frag20.x[i];
       c_frag30.x[i] = K - c_frag30.x[i];
-      //c_frag40.x[i] = K - c_frag40.x[i];
-      //c_frag50.x[i] = K - c_frag50.x[i];
-      //c_frag60.x[i] = K - c_frag60.x[i];
-      //c_frag70.x[i] = K - c_frag70.x[i];
+      c_frag40.x[i] = K - c_frag40.x[i];
+      c_frag50.x[i] = K - c_frag50.x[i];
+      c_frag60.x[i] = K - c_frag60.x[i];
+      c_frag70.x[i] = K - c_frag70.x[i];
    }
 
    wmma::store_matrix_sync(c + cRow0 * ldc + cCol0, c_frag00, ldc, wmma::mem_row_major);
    wmma::store_matrix_sync(c + cRow1 * ldc + cCol0, c_frag10, ldc, wmma::mem_row_major);
    wmma::store_matrix_sync(c + cRow2 * ldc + cCol0, c_frag20, ldc, wmma::mem_row_major);
    wmma::store_matrix_sync(c + cRow3 * ldc + cCol0, c_frag30, ldc, wmma::mem_row_major);
-   //wmma::store_matrix_sync(c + cRow4 * ldc + cCol0, c_frag40, ldc, wmma::mem_row_major);
-   //wmma::store_matrix_sync(c + cRow5 * ldc + cCol0, c_frag50, ldc, wmma::mem_row_major);
-   //wmma::store_matrix_sync(c + cRow6 * ldc + cCol0, c_frag60, ldc, wmma::mem_row_major);
-   //wmma::store_matrix_sync(c + cRow7 * ldc + cCol0, c_frag70, ldc, wmma::mem_row_major);
+   wmma::store_matrix_sync(c + cRow4 * ldc + cCol0, c_frag40, ldc, wmma::mem_row_major);
+   wmma::store_matrix_sync(c + cRow5 * ldc + cCol0, c_frag50, ldc, wmma::mem_row_major);
+   wmma::store_matrix_sync(c + cRow6 * ldc + cCol0, c_frag60, ldc, wmma::mem_row_major);
+   wmma::store_matrix_sync(c + cRow7 * ldc + cCol0, c_frag70, ldc, wmma::mem_row_major);
    
 }
 
@@ -174,7 +174,7 @@ void quantWmmaUnrollV2(Data data, Setting setting) {
    blockDim.x = 128;
    blockDim.y = 4;
 
-   int unitX = WMMA_M * blockDim.x / 32 * 4;
+   int unitX = WMMA_M * blockDim.x / 32 * 8;
    int unitY = WMMA_N * blockDim.y * 1;
 
    gridDim.x = (MATRIX_M + unitX - 1) / unitX;
