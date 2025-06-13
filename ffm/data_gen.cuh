0ffa8f2f108d64898be1346f873090e91735371a
Yuchin Juan <juan.yuchin@gmail.com>
2025-06-13 21:40:10 +0000

    .

---
+++ ffm/data_gen.cuh
@@ -0,0 +1,54 @@
+#ifndef DATA_GEN_CUH
+#define DATA_GEN_CUH
+
+#include <vector>
+#include <random>
+
+#include "data_struct.cuh"
+
+std::vector<std::vector<std::vector<float>>> genRandFFMData(int numRows, int numFields, int embDimPerField)
+{
+    using namespace std;
+
+    default_random_engine generator;
+    uniform_real_distribution<float> distribution(0, sqrt(1.0f / embDimPerField));
+
+    vector<vector<vector<float>>> data(numRows, vector<vector<float>>(numFields, vector<float>(embDimPerField)));
+
+    for (int i = 0; i < numRows; ++i)
+    {
+        for (int j = 0; j < numFields; ++j)
+        {
+            for (int k = 0; k < embDimPerField; ++k)
+            {
+                data[i][j][k] = distribution(generator);
+            }
+        }
+    }
+
+    return data;
+}
+
+std::vector<ScoringTask> genRandScoringTasks(int numReqs, int numDocsPerReqs, int numTotalDocs)
+{
+    using namespace std;
+
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0, numTotalDocs - 1);
+
+    vector<ScoringTask> tasks(numReqs * numDocsPerReqs);
+    for (int reqIdx = 0; reqIdx < numReqs; ++reqIdx)
+    {
+        for (int docIdx = 0; docIdx < numDocsPerReqs; ++docIdx)
+        {
+            int taskIdx = reqIdx * numDocsPerReqs + docIdx;
+            tasks[taskIdx].reqIdx = reqIdx;
+            tasks[taskIdx].docIdx = distribution(generator);
+            tasks[taskIdx].result = 0.0f;
+        }
+    }
+
+    return tasks;
+}
+
+#endif // DATA_GEN_CUH
---
+++ ffm/main.cu
@@ -0,0 +1,4 @@
+#include <cuda_runtime.h>
+#include "ffm_gpu.cuh"
+
+
