578b0e23f00e2a9e7660e01ff6743500a88d3aec
Yuchin Juan <juan.yuchin@gmail.com>
2025-07-18 07:02:06 +0000

    move genData to main

--- gemm/data.cuh
+++ gemm/data.cuh
@@ -22,17 +22,8 @@ enum MemLayout
     COL_MAJOR
 };
 
-int kNumDocs = 1 << 20;
-int kNumReqs = 1 << 0;
-int kEmbDim = 1 << 10;
-int kNumTrials = 100;
-
-MemLayout kMemLayoutDoc = COL_MAJOR;
-MemLayout kMemLayoutReq = ROW_MAJOR;
-MemLayout kMemLayoutRstCpu = COL_MAJOR;
-MemLayout kMemLayoutRstGpuCuda = COL_MAJOR;
-MemLayout kMemLayoutRstGpuCublas = COL_MAJOR;
-
+// Important: Having if-else here is going to cause warp divergence problem.
+// For performance-critical code, please write another hard-coded version (without if-else).
 __device__ __host__ size_t getMemAddr(int i, int j, int M, int N, MemLayout layout)
 {
     if (layout == ROW_MAJOR)
@@ -79,35 +70,5 @@ struct Data
         cout << oss.str();
     }
 };
-
-template <typename T>
-Data<T> genData()
-{
-    Data<T> data;
-    data.numDocs = kNumDocs;
-    data.numReqs = kNumReqs;
-    data.embDim = kEmbDim;
-    data.docMemLayout = kMemLayoutDoc;
-    data.reqMemLayout = kMemLayoutReq;
-    data.rstLayoutCpu = kMemLayoutRstCpu;
-    data.rstLayoutGpuKernel = kMemLayoutRstGpuCuda;
-    data.rstLayoutGpuCublas = kMemLayoutRstGpuCublas;
-    data.print();
-    
-    CHECK_CUDA(cudaMallocManaged(&data.d_doc, (size_t)data.numDocs * data.embDim * sizeof(T)));
-    CHECK_CUDA(cudaMallocManaged(&data.d_req, (size_t)data.numReqs * data.embDim * sizeof(T)));
-    CHECK_CUDA(cudaMallocManaged(&data.d_rst_kernel, (size_t)data.numDocs * data.numReqs * sizeof(float)));
-    CHECK_CUDA(cudaMallocManaged(&data.d_rst_cublas, (size_t)data.numDocs * data.numReqs * sizeof(float)));
-    CHECK_CUDA(cudaMallocHost(&data.h_rst_cpu, (size_t)data.numDocs * data.numReqs * sizeof(float)));
-
-    default_random_engine generator;
-    uniform_real_distribution<float> distribution(0.0, 1.0);
-    for (int i = 0; i < data.numDocs * data.embDim; i++)
-        data.d_doc[i] = (T)distribution(generator);
-    for (int i = 0; i < data.numReqs * data.embDim; i++)
-        data.d_req[i] = (T)distribution(generator);
-
-    return data;
-}
 
 #endif
