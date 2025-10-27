c35b759efa3915508f0ae639167f3545cf81e547
Yuchin Juan <juan.yuchin@gmail.com>
2025-10-26 17:47:35 -0700

    polish code

--- cabm/cabm_gpu.cu
+++ cabm/cabm_gpu.cu
@@ -11,14 +11,29 @@
 #include "util.cuh"
 
 // We use uint64_t to store the bit stack, so the max number of elements is 64
-constexpr uint32_t g_kMaxBitStackCount = 64;
+constexpr uint32_t g_kMaxBitStackSize = 64;
 
+/*
+  Let's say we want to push "True" to the bit stack at index 3.
+  The mask is 1L << 3 = 0000000000000000000000000000000000000000000000000000000000001000
+  bitStack | mask will ensure the 3rd bit is set to 1, while keeping the other bits unchanged.
+
+  Let's say we want to push "False" to the bit stack at index 4.
+  The mask is ~(1L << 4) = 1111111111111111111111111111111111111111111111111111111111101111
+  bitStack & mask will ensure the 4th bit is set to 0, while keeping the other bits unchanged.
+*/
 __device__ void stackPush(uint64_t& bitStack, const uint8_t bitStackIdx, bool value)
 {
     uint64_t mask = value? 1L << bitStackIdx : ~(1L << bitStackIdx);
     bitStack = value? bitStack | mask : bitStack & mask;
 }
 
+/*
+  Let's say we want to get the top value of the bit stack at index 5.
+  The mask is 1L << 5 = 0000000000000000000000000000000000000000000000000000000000100000
+  bitStack & mask will ensure the 5th bit is kept, while the other bits are set to 0.
+  This way, if the 5th bit is 1, then tmp > 0L, otherwise tmp is 0L.
+*/
 __device__ bool stackTop(const uint64_t bitStack, const uint8_t bitStackIdx)
 {
     uint64_t mask = 1L << bitStackIdx;
@@ -32,32 +47,39 @@ __device__ bool matchOp(const AbmDataGpu& reqAbmDataGpu,
                         const int docIdx,
                         const CabmOp& op)
 {
+    // Get the offset iterators 
     int reqOffsetIter = reqAbmDataGpu.getOffset(reqIdx, op.getReqFieldIdx());
     int docOffsetIter = docAbmDataGpu.getOffset(docIdx, op.getDocFieldIdx());
+
+    // Get the end offset iterators
     int reqOffsetEnd = reqAbmDataGpu.getOffset(reqIdx, op.getReqFieldIdx() + 1);
     int docOffsetEnd = docAbmDataGpu.getOffset(docIdx, op.getDocFieldIdx() + 1);
 
     bool rst = false;
-    while (reqOffsetIter < reqOffsetEnd && docOffsetIter < docOffsetEnd)
+    // We assume the req and doc data are sorted.
+    while (reqOffsetIter < reqOffsetEnd && docOffsetIter < docOffsetEnd) // While the iterators are not at the end
     {
+        // Get the values
         long reqVal = reqAbmDataGpu.getVal(reqIdx, reqOffsetIter);
         long docVal = docAbmDataGpu.getVal(docIdx, docOffsetIter);
+
+        // If the values are equal, we have a match, so we can break.
         if (reqVal == docVal)
         {
             rst = true;
             break;
         }
-        else if (reqVal < docVal)
+        else if (reqVal < docVal) // If the req value is less than the doc value, we increment the req iterator.
         {
             reqOffsetIter++;
         }
-        else
+        else // If the req value is greater than the doc value, we increment the doc iterator.
         {
             docOffsetIter++;
         }
     }
 
-    if (op.isNegation())
+    if (op.isNegation()) // If the operand is negated, we negate the result.
     {
         rst = !rst;
     }
@@ -101,10 +123,10 @@ __global__ void operatorKernel(OperatorKernelParam param)
     if (docIdx < param.numDocs)
     {
         uint64_t& bitStack = param.d_bitStacks[docIdx];
-        bool rst1 = stackTop(bitStack, param.bitStackIdx - 1);
-        bool rst2 = stackTop(bitStack, param.bitStackIdx - 2);
-        bool rst = (param.op.getOpType() == CabmOpType::OPERATOR_AND) ? (rst1 & rst2) : (rst1 | rst2);
-        stackPush(bitStack, param.bitStackIdx - 2, rst);
+        bool rst1 = stackTop(bitStack, param.bitStackIdx - 1); // Get the first of first operand
+        bool rst2 = stackTop(bitStack, param.bitStackIdx - 2); // Get the second of second operand
+        bool rst = (param.op.getOpType() == CabmOpType::OPERATOR_AND) ? (rst1 & rst2) : (rst1 | rst2); // Apply the operator
+        stackPush(bitStack, param.bitStackIdx - 2, rst); // Push the result to the bit stack
     }
 }
 
@@ -126,28 +148,39 @@ void cabmGpu(CabmGpuParam& param)
     param.timeMsCopyRstKernel = 0;
     param.timeMsTotal = 0;
 
+    // -----------------
+    // Start timer
     Timer timerTotal;
     timerTotal.tic();
 
+    // -----------------
+    // Set the block and grid size
     const int kBlockSize = 1024;
     const int kGridSize = (param.numDocs + kBlockSize - 1) / kBlockSize;
 
     for (uint32_t reqIdx = 0; reqIdx < param.numReqs; reqIdx++)
     {
+        // -----------------
+        // Initialize the bit stack index and set the bit stack to 0
         uint8_t currBitStackIdx = 0;
         CHECK_CUDA(cudaMemset(param.d_bitStacks, 0, param.numDocs * sizeof(uint64_t)));
+        
         for (const auto& op : param.postfixOps)
         {
             if (op.isOperand())
             {
-                if (currBitStackIdx >= g_kMaxBitStackCount)
+                // -----------------
+                // Check if the bit stack index is greater than the maximum bit stack size
+                if (currBitStackIdx >= g_kMaxBitStackSize)
                 {
                     std::ostringstream oss;
                     oss << "currBitStackIdx is greater than g_kMaxBitStackCount: " << (int)currBitStackIdx
-                        << " >= " << g_kMaxBitStackCount;
+                        << " >= " << g_kMaxBitStackSize;
                     throw std::runtime_error(oss.str());
                 }
 
+                // -----------------
+                // Create the operand kernel parameter
                 OperandKernelParam operandKernelParam;
                 operandKernelParam.reqAbmDataGpu = param.reqAbmDataGpu;
                 operandKernelParam.docAbmDataGpu = param.docAbmDataGpu;
@@ -157,6 +190,8 @@ void cabmGpu(CabmGpuParam& param)
                 operandKernelParam.d_bitStacks = param.d_bitStacks;
                 operandKernelParam.bitStackIdx = currBitStackIdx;
 
+                // -----------------
+                // Launch the operand kernel
                 Timer timerOperandKernel;
                 timerOperandKernel.tic();
                 if (op.getOpType() == CabmOpType::OPERAND_MATCH)
@@ -172,16 +207,24 @@ void cabmGpu(CabmGpuParam& param)
                 CHECK_CUDA(cudaDeviceSynchronize());
                 CHECK_CUDA(cudaGetLastError());
                 param.timeMsOperandKernel += timerOperandKernel.tocMs();
-                currBitStackIdx++; // We push 1, so the net effect is +1
+
+                // -----------------
+                // Increment the bit stack index
+                // We push 1, so the net effect is +1
+                currBitStackIdx++;
             }
             else if (op.isOperator())
             {
+                // -----------------
+                // Create the operator kernel parameter
                 OperatorKernelParam operatorKernelParam;
                 operatorKernelParam.op = op;
                 operatorKernelParam.d_bitStacks = param.d_bitStacks;
                 operatorKernelParam.bitStackIdx = currBitStackIdx;
                 operatorKernelParam.numDocs = param.numDocs;
 
+                // -----------------
+                // Launch the operator kernel
                 Timer timerOperatorKernel;
                 timerOperatorKernel.tic();
                 operatorKernel<<<kGridSize, kBlockSize>>>(operatorKernelParam);
@@ -189,10 +232,15 @@ void cabmGpu(CabmGpuParam& param)
                 CHECK_CUDA(cudaGetLastError());
                 param.timeMsOperatorKernel += timerOperatorKernel.tocMs();
 
-                currBitStackIdx--; // We pop 2 and push 1, so the net effect is -1
+                // -----------------
+                // Decrement the bit stack index
+                // We pop 2 and push 1, so the net effect is -1
+                currBitStackIdx--;
             }
         }
 
+        // -----------------
+        // And the end of postfix evaluation, there should be exactly one value in the bit stack.
         if (currBitStackIdx != 1)
         {
             std::ostringstream oss;
@@ -200,6 +248,8 @@ void cabmGpu(CabmGpuParam& param)
             throw std::runtime_error(oss.str());
         }
 
+        // -----------------
+        // Copy the result to the output
         Timer timerCopyRstKernel;
         timerCopyRstKernel.tic();
         copyRstKernel<<<kGridSize, kBlockSize>>>(param.d_rst, param.d_bitStacks, param.numDocs, reqIdx);
@@ -208,6 +258,8 @@ void cabmGpu(CabmGpuParam& param)
         param.timeMsCopyRstKernel += timerCopyRstKernel.tocMs();
     }
 
+    // -----------------
+    // Stop timer
     param.timeMsTotal = timerTotal.tocMs();
 }
 
