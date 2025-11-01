e050e8d3734586ac30ba1495565ae9b928063762
Yuchin Juan <juan.yuchin@gmail.com>
2025-11-01 12:48:01 -0700

    .

--- topk_with_bucket_sort/topk_gpu.cuh
+++ topk_with_bucket_sort/topk_gpu.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <random>
 #include <vector>
 #include <thrust/copy.h>
 #include <thrust/execution_policy.h>
@@ -23,19 +24,46 @@ __global__ void updateCounterKernel(T* d_doc, int numDocs, TopkBucketSort<T, Sco
     }
 }
 
+template<typename T, class ScoreExtractor>
+__global__ void sampleRandomScoresKernel(T* d_doc, int numToSample, float* d_sampledScores, uint32_t* d_randomIndices)
+{
+    int docId = blockIdx.x * blockDim.x + threadIdx.x;
+    if (docId < numToSample)
+    {
+        d_sampledScores[docId] = ScoreExtractor()(d_doc[d_randomIndices[docId]]);
+    }
+}
+
 template <typename T, class ScorePredicator, class DocIdExtractor, class ScoreExtractor> class TopkBucketSort
 {
 public:
     void init()
     {
+        shouldCheckMinMaxScore_ = true;
         CHECK_CUDA(cudaMalloc(&d_counter_, kNumBuckets_ * kNumSlots_ * sizeof(int)));
-        CHECK_CUDA(cudaMallocHost(&h_counter_, kNumBuckets_ * kNumSlots_ * sizeof(int)));
+        CHECK_CUDA(cudaMallocHost(&hp_counter_, kNumBuckets_ * kNumSlots_ * sizeof(int)));
+        CHECK_CUDA(cudaMalloc(&d_randomIndices_, kNumDocsToSample_ * sizeof(uint32_t)));
+        CHECK_CUDA(cudaMallocHost(&hp_randomIndices_, kNumDocsToSample_ * sizeof(uint32_t)));
+        CHECK_CUDA(cudaMalloc(&d_sampledScores_, kNumDocsToSample_ * sizeof(float)));
+        CHECK_CUDA(cudaMallocHost(&hp_sampledScores_, kNumDocsToSample_ * sizeof(float)));
+    }
+
+    void init(float minScore, float maxScore)
+    {
+        minScore_ = minScore;
+        maxScore_ = maxScore;
+        init();
+        shouldCheckMinMaxScore_ = false; // Since the caller has already provided the min and max score, we don't need to check again.
     }
 
     void reset()
     {
         CHECK_CUDA(cudaFree(d_counter_));
-        CHECK_CUDA(cudaFreeHost(h_counter_));
+        CHECK_CUDA(cudaFreeHost(hp_counter_));
+        CHECK_CUDA(cudaFree(d_randomIndices_));
+        CHECK_CUDA(cudaFreeHost(hp_randomIndices_));
+        CHECK_CUDA(cudaFree(d_sampledScores_));
+        CHECK_CUDA(cudaFreeHost(hp_sampledScores_));
     }
 
     std::vector<T> retrieveTopk(T* d_doc, T* d_buffer, int numDocs, int numToRetrieve, float& timeMs)
@@ -45,6 +73,12 @@ public:
     
         int kBlockSize = 256;
         int gridSize = (int)ceil((double)(numDocs + 1) / kBlockSize);
+
+        if (shouldCheckMinMaxScore_)
+        {
+            checkMinMaxScore(d_doc, numDocs);
+            //std::cout << "Min score: " << minScore_ << ", Max score: " << maxScore_ << std::endl;
+        }
     
         // Step1 - Run kernel to update the counter in each bucket
         CHECK_CUDA(cudaMemset(d_counter_, 0, kSize_byte_d_counter_))
@@ -94,8 +128,8 @@ public:
     // Convert the score to the bucket index.
     __device__ int bucketize(float score)
     {
-        score = min(kMaxScore_, max(kMinScore_, score));
-        score = (score - kMinScore_) / (kMaxScore_ - kMinScore_);
+        score = min(maxScore_, max(minScore_, score));
+        score = (score - minScore_) / (maxScore_ - minScore_);
         return (int)(score * kGranularity_);
     }
 
@@ -116,8 +150,6 @@ public:
     }
 
 private:
-    const float kMinScore_ = -1;
-    const float kMaxScore_ = 1;
     const int kGranularity_ = 512;
     const int kNumBuckets_ = kGranularity_ + 1;
     // For example, if we have kMinScore = -1.0, kMaxScore = 1.0, kGranularity = 2,
@@ -129,8 +161,22 @@ private:
     const int kSize_d_counter_ = kNumBuckets_ * kNumSlots_;
     const int kSize_byte_d_counter_ = kSize_d_counter_ * sizeof(int);
 
+    // --------------
+    // Sampling related Min and max score of the docs
+    float minScore_ = -1;
+    float maxScore_ = 1;
+    bool shouldCheckMinMaxScore_ = true; // When this is set to true, the algorithm will run an additional step 
+                                         // to sample some docs to get the min and max score.
+    const uint32_t kNumDocsToSample_ = 10000;
+    uint32_t* d_randomIndices_ = nullptr;
+    uint32_t* hp_randomIndices_ = nullptr;
+    float *d_sampledScores_ = nullptr;
+    float *hp_sampledScores_ = nullptr;
+    float maxPercentile_ = 0.99;
+    float minPercentile_ = 0.01;
+
     int* d_counter_ = nullptr;
-    int* h_counter_ = nullptr;
+    int* hp_counter_ = nullptr;
     int lowestBucket_ = 0;
 
     void findLowestBucket(std::vector<int>& v_counter,
@@ -158,5 +204,42 @@ private:
                 break;
             }
         }
+    }
+
+    void checkMinMaxScore(T* d_doc, uint32_t numDocs)
+    {
+        // --------------
+        // Step1 - Sample some random indices
+        std::default_random_engine generator;
+        std::uniform_int_distribution<uint32_t> distribution(0, numDocs - 1);
+        for (int i = 0; i < kNumDocsToSample_; i++)
+        {
+            hp_randomIndices_[i] = distribution(generator);
+        }
+
+        // --------------
+        // Step2 - Copy the random indices to GPU
+        CHECK_CUDA(cudaMemcpy(d_randomIndices_, hp_randomIndices_, kNumDocsToSample_ * sizeof(uint32_t), cudaMemcpyHostToDevice));
+
+        // --------------
+        // Step3 - Sample the scores
+        int kBlockSize = 256;
+        int gridSize = (int)ceil((double)(numDocs + 1) / kBlockSize);
+        int numToSample = std::min(numDocs, kNumDocsToSample_);
+        sampleRandomScoresKernel<T, ScoreExtractor>
+            <<<gridSize, kBlockSize>>>(d_doc, numToSample, d_sampledScores_, d_randomIndices_);
+        CHECK_CUDA(cudaDeviceSynchronize());
+        CHECK_CUDA(cudaGetLastError())
+
+        // --------------
+        // Step4 - Sort the scores in GPU
+        thrust::sort(thrust::device, d_sampledScores_, d_sampledScores_ + kNumDocsToSample_, thrust::less<float>());
+
+        // --------------
+        // Step5 - Copy the scores back to CPU
+        CHECK_CUDA(cudaMemcpy(hp_sampledScores_, d_sampledScores_, kNumDocsToSample_ * sizeof(float), cudaMemcpyDeviceToHost));
+
+        minScore_ = hp_sampledScores_[(int)(kNumDocsToSample_ * minPercentile_)];
+        maxScore_ = hp_sampledScores_[(int)(kNumDocsToSample_ * maxPercentile_)];
     }
 };
