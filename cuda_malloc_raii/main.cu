684809197d2ae53573d6a78c2996b0626ed18b91
Yuchin Juan <juan.yuchin@gmail.com>
2026-01-14 20:21:01 -0800

    one more tests

--- cuda_malloc_raii/main.cu
+++ cuda_malloc_raii/main.cu
@@ -21,6 +21,7 @@ int main()
     // --------------
     // Test CudaDeviceArray
     {
+        std::cout << "======== Test CudaDeviceArray ========" << std::endl;
         size_t freeMemBeforeMalloc = getFreeMemoryInBytes();
         {
             CudaDeviceArray<float> arr(kArraySize, "arr1");
@@ -37,6 +38,7 @@ int main()
     // --------------
     // Test CudaHostArray
     {
+        std::cout << "======== Test CudaHostArray ========" << std::endl;
         // It's a bit hard to use the free memory to assert. So we will just eye-ball the logs.
         CudaHostArray<float> arr(kArraySize, "arr1");
     }
@@ -44,11 +46,29 @@ int main()
     // --------------
     // Test Polymorphism
     {
+        std::cout << "======== Test Polymorphism ========" << std::endl;
         size_t freeMemBeforeMalloc = getFreeMemoryInBytes();
         CudaArray<float> *arr = new CudaDeviceArray<float>(kArraySize, "arr1");
         delete arr;
         size_t freeMemAfterFree = getFreeMemoryInBytes();
         assert(freeMemBeforeMalloc == freeMemAfterFree); // Make sure the destructor of the derived class is called
+    }
+
+    // --------------
+    // Test copy
+    {
+        std::cout << "======== Test copy ========" << std::endl;
+        size_t freeMemBeforeMalloc = getFreeMemoryInBytes();
+        {
+            CudaDeviceArray<float> arr1(kArraySize, "arr1");
+            {
+                auto arr2 = arr1;
+            }
+            size_t freeMemAfterArr2Free = getFreeMemoryInBytes();
+            assert(freeMemAfterArr2Free < freeMemBeforeMalloc); // arr2 is out of scope, but arr1 is still in scope, so the memory should not be freed
+        }
+        size_t freeMemAfterArr1Free = getFreeMemoryInBytes();
+        assert(freeMemAfterArr1Free == freeMemBeforeMalloc); // arr1 is out of scope too, so the memory should be freed
     }
 
     return 0;
