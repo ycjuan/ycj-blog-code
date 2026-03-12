5d79df78adcf451aa9b404815c79730d6cc06cb6
Yuchin Juan <juan.yuchin@gmail.com>
2026-03-12 06:16:10 -0700

    ctor

--- async_memcpy_benchmark/include/data/emb_data.hpp
+++ async_memcpy_benchmark/include/data/emb_data.hpp
@@ -3,12 +3,15 @@
 #include "common/typedef.hpp"
 #include "utils/cuda_raii.hpp"
 
-class EmbData {
+class EmbData
+{
 public:
     EmbData(int numDocs, int embDim);
+
+    const T_EMB* data() const;
 
 private:
     int numDocs;
     int embDim;
-    CudaDeviceArray<T_EMB> data;
+    CudaDeviceArray<T_EMB> d_data;
 };
--- async_memcpy_benchmark/src/data/emb_data.cu
+++ async_memcpy_benchmark/src/data/emb_data.cu
@@ -1,4 +1,5 @@
 #include "data/emb_data.hpp"
+#include "utils/util.hpp"
 
 #include <random>
 #include <vector>
@@ -6,7 +7,7 @@
 EmbData::EmbData(int numDocs, int embDim)
     : numDocs(numDocs)
     , embDim(embDim)
-    , data(numDocs * embDim, "EmbData")
+    , d_data(numDocs * embDim, "EmbData")
 {
     const int totalSize = numDocs * embDim;
 
@@ -14,9 +15,12 @@ EmbData::EmbData(int numDocs, int embDim)
     std::normal_distribution<float> dist(0.0f, 1.0f);
 
     std::vector<T_EMB> hostData(totalSize);
-    for (auto& val : hostData) {
-        val = __float2bfloat16(dist(rng));
+    for (auto& val : hostData)
+    {
+        val = static_cast<T_EMB>(dist(rng));
     }
 
-    cudaMemcpy(data.data(), hostData.data(), totalSize * sizeof(T_EMB), cudaMemcpyHostToDevice);
+    CHECK_CUDA(cudaMemcpy(d_data.data(), hostData.data(), totalSize * sizeof(T_EMB), cudaMemcpyHostToDevice));
 }
+
+const T_EMB* EmbData::data() const { return d_data.data(); }
