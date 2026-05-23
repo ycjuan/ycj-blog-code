c15938c06b22973b484e249eba9944d426b460a0
Yuchin Juan <juan.yuchin@gmail.com>
2026-05-23 10:26:44 -0700

    auto-commit after edit

--- async_memcpy_benchmark/include/worker_naive.hpp
+++ async_memcpy_benchmark/include/worker_naive.hpp
@@ -7,5 +7,6 @@ class WorkerNaive : public Worker
 public:
     WorkerNaive(int maxNumDocs, int embDim);
 
-    void update(const std::vector<long>& jobIds, const std::vector<std::vector<T_EMB>>& embData2D) override;
+    void updateEmbData(const std::vector<long>& jobIds, const std::vector<std::vector<T_EMB>>& embData2D) override;
+    void updateScalarData(const std::vector<long>& jobIds, const std::vector<float>& scalars) override;
 };
